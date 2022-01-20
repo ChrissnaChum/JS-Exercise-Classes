156336a9b05ec8f38108a067d204b7fc968e7087
Chrissna Chum <cchum12@gmail.com>
2022-01-20 17:13:17 -0500

    complete mvp

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ class ProjectManager extends Instructor{
     - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
     - Add a graduate method to a student.
       + This method, when called, will check the grade of the student and see if they're ready to graduate from BloomTech
-      + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+      + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their scoree
 */
 
 
